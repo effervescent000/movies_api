58d309a16bc993a3c742ae319d2bd5f221739f9d
effervescent000 <kislio01@gmail.com>
2021-11-29 14:03:33 -0500

    setting up db/schemas

--- app.py
+++ app.py
@@ -11,5 +11,28 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    genre = db.Column(db.String, nullable=False)
+    mpaa_rating = db.Column(db.String)
+    poster_image = db.Column(db.String, unique=True)
+
+    def __init__(self, title, genre, mpaa_rating, poster_image):
+        self.title = title
+        self.genre = genre
+        self.mpaa_rating = mpaa_rating
+        self.poster_image = poster_image
+
+
+class MovieSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'genre', 'mpaa_rating', 'poster_image')
+
+
+movie_schema = MovieSchema()
+multiple_movies_schema = MovieSchema(many=True)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
